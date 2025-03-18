c151ac2443a5c883895c5049904db375ac08dbe8
edupoch <edupoch@gmail.com>
2025-03-18 11:14:03 +0100

    Axustes no son do salto e merge

--- Assets/Scripts/Player/StateMachine/AirState.cs
+++ Assets/Scripts/Player/StateMachine/AirState.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class AirState : State
 {
@@ -8,7 +9,14 @@ public class AirState : State
     private float _verticalVelocity;
     private bool _isFalling;
     private bool _lastIsFalling;
-    
+
+    private StudioEventEmitter _jumpSoundEmitter;
+
+    private void Awake()
+    {
+        _jumpSoundEmitter = GetComponent<StudioEventEmitter>();
+    }
+
     public override void Enter()
     {
         _verticalVelocity = Body.linearVelocity.y;
@@ -16,6 +24,8 @@ public class AirState : State
         StateAnimator.Play(_isFalling ? "Fall" : "Jump");
 
         _lastIsFalling = _isFalling;
+
+        _jumpSoundEmitter.Play();
     }
 
     public override void Do()
@@ -31,7 +41,7 @@ public class AirState : State
 
         if (_isFalling && _lastIsFalling != _isFalling) StateAnimator.Play("TrJumpFall");
         else if (!_isFalling) StateAnimator.Play("Jump");
-        
+
         _lastIsFalling = _isFalling;
     }
 
